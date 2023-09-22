9d3aa3fa54c4734c2b8a96aaee5969d328ec3661
odhiambo-ed <odhiambo.edward1112gmail.com>
2023-09-22 13:38:19 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header, Footer } from './components'
+import { Home, Contact, Cart, Order, Admin } from './pages'
 
 function App() {
   return (
@@ -8,7 +9,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          
+          <Route path='/' element={ <Home /> } />
+          <Route path='/contact' element={ <Contact /> } />
+          <Route path='/cart' element={ <Cart /> } />
+          <Route path='/order' element={ <Order /> } />
+          <Route path='/Admin' element={ <Admin /> } />
         </Routes>
         <Footer />
       </BrowserRouter>
